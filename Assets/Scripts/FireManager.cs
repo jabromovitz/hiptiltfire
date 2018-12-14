54240603940c60238b2163dd7ed0623d047b654d
Jason Abromovitz <jabromovitz@gmail.com>
2018-12-14 13:16:30 -0800

    fire manager and smoke effect

--- Assets/Scripts/FireManager.cs
+++ Assets/Scripts/FireManager.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class FireManager : MonoBehaviour {
 
 	public Transform fire;
+	private Flamable fireScript;
 	public Transform floor;
-	private static float CENTER_ZONE_CUSHION = 0.0f;
-	Queue<Vector3> firePosQ = new Queue<Vector3>();
+	private static float CENTER_ZONE_CUSHION = 20.0f;
+	private Queue<Vector3> firePosQ = new Queue<Vector3>();
 
 	void Start () {
+
+		fireScript = fire.GetComponent<Flamable>();
+		fireScript.onFlameExtinguished += onFireExtenguised;
 		Init(4, 4);
 	}
 	
@@ -21,25 +25,42 @@ public class FireManager : MonoBehaviour {
 	public void Init (int zones, int repsPerZone) {
 
 		// Populate fire position queue
+		
 		for (int i = 1; i <= zones; i++) {
 
 			float increment = (1 - 0.01f * CENTER_ZONE_CUSHION/2) / (zones * 2);
 			float pos = 0.01f * CENTER_ZONE_CUSHION/2 + (2 * i - 1) * increment;
 
-			for (int j = 0; i < repsPerZone; j++) {
+			for (int j = 0; j < repsPerZone; j++) {
+
 				firePosQ.Enqueue(new Vector3(0, 0, -1.0f * pos * 5f));
 				firePosQ.Enqueue(new Vector3(0, 0, 1.0f * pos * 5f));
 			}
 		}
+		
+		// Set first fire
+		SetNextFire();
+		
+		
 	}
 
 	private void SetNextFire() {
-		
+
+		fireScript.Reset();
 		fire.localPosition = firePosQ.Dequeue();
 	}
 
 	private bool isFireQEmpty () {
 		
 		return firePosQ.Count == 0;
+	}
+
+	private void onFireExtenguised () {
+		Debug.Log("Fire's out");
+		if (!isFireQEmpty()) {
+			
+			SetNextFire();
+		}
+		
 	}
 }
--- Assets/Scripts/Flamable.cs
+++ Assets/Scripts/Flamable.cs
@@ -1,29 +1,39 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
  public class Flamable : MonoBehaviour {
      
-     public float FireHealth = 50;
-     public float MaxFireHealth = 50;
-     public float HealthRegen = 5;
-     public bool IsOnFire = true;
-     
+     public float fireHealth = 50;
+     public float maxFireHealth = 50;
+     public float healthRegen = 5;
+     public bool isOnFire = true;
+     //Defining Delegate
+    public event Action onFlameExtinguished;
+
+    
+    public void Reset() {
+
+        fireHealth = maxFireHealth;
+        isOnFire = true;
+    }
      void OnParticleCollision(GameObject other) {       
-         if(IsOnFire) {
-             FireHealth -= 1.0f;
-             if (FireHealth <= 0) {
-                 IsOnFire = false;
-                 gameObject.SetActive(false);
+         if(isOnFire) {
+             fireHealth -= 1.0f;
+             if (fireHealth <= 0) {
+                isOnFire = false;
+                onFlameExtinguished();
+                //gameObject.SetActive(false);
              }
          }
      }
 
      void Update() {
-         if (IsOnFire) {
-             FireHealth += Time.deltaTime * HealthRegen;    
-             if (FireHealth > MaxFireHealth) {
-                 FireHealth = MaxFireHealth;
+         if (isOnFire) {
+             fireHealth += Time.deltaTime * healthRegen;    
+             if (fireHealth > maxFireHealth) {
+                 fireHealth = maxFireHealth;
              }
          }
      }
